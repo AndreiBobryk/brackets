adc12950c1eedcf6281f7554e851bb96937c5639
Andrei Bobryk <andreibobryk1@gmail.com>
2022-01-30 00:12:58 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+ module.exports = function check(str, bracketsConfig) {
+
+  let arr = bracketsConfig.map((item) => {
+    return item.join("");
+  });
+
+  let strTemp = str;
+  let i = 0;
+
+  let count = 0;
+
+  while (count < arr.length) {
+    if (str.indexOf(arr[i]) !== -1) {
+      str =
+      str.slice(0, str.indexOf(arr[i])) + str.slice(str.indexOf(arr[i]) + 2);
+        console.log(strTemp)
+        count =0;
+        i=0;
+    } else {
+      count++;
+      i++;
+    }
+    if (i === arr.length) {
+      i = 0;
+    }
+  }
+
+  if(str) {
+    return false;
+  } else {
+    return true;
+  }
+
+
 }
